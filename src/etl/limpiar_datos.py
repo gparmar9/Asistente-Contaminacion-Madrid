5596610f4d659edacbe2c745d6fe35974662108d
guille.pares <guillepm09@gmail.com>
2026-05-07 14:02:08 +0200

    Workflow para programar la ingesta de datos y cambios en el script de ingesta para quedarnos solo con 6 contaminantes

--- src/etl/limpiar_datos.py
+++ src/etl/limpiar_datos.py
@@ -5,11 +5,13 @@ import os
 
 # Diccionario de magnitudes
 DICCIONARIO_MAGNITUDES = {
-    1: 'SO2', 6: 'CO', 7: 'NO', 8: 'NO2', 9: 'PM2.5', 
+    1: 'SO2', 6: 'CO', 7: 'NO', 8: 'NO2', 9: 'PM2.5',
     10: 'PM10', 12: 'NOx', 14: 'O3', 20: 'TOL', 30: 'BEN',
     35: 'EBE', 37: 'MXY', 38: 'PXY', 39: 'OXY', 42: 'TCH',
     43: 'CH4', 44: 'NMHC', 431: 'MPX'
 }
+
+CONTAMINANTES_OBJETIVO = {'NO', 'NO2', 'NOx', 'O3', 'PM10', 'PM2.5'}
 
 def limpiar_datos_csv(ruta_input, ruta_estaciones):
     
@@ -23,27 +25,17 @@ def limpiar_datos_csv(ruta_input, ruta_estaciones):
     # Columnas que queremos despivotar
     cols_h = [f'H{str(i).zfill(2)}' for i in range(1, 25)]
     cols_v = [f'V{str(i).zfill(2)}' for i in range(1, 25)]
-    print(cols_h)
-    print(cols_v)
 
     # Despivotar valores
     df_values = df.melt(id_vars=id_vars, value_vars=cols_h, var_name='hora_raw', value_name='valor')
-    print('HORAS')
-    print(df_values.head())
     
     # Despivotar validaciones
     df_valid = df.melt(id_vars=id_vars, value_vars=cols_v, var_name='val_raw', value_name='es_valido')
-    print('\n')
-    print('VALIDACIONES')
-    print(df_valid.head())
     
     # Crear dataframe final y unir columna de valicación
     df_final = df_values.copy()
     df_final.columns = [c.lower() for c in df_final.columns]
     df_final['validacion'] = df_valid['es_valido']
-    print('\n')
-    print('FINAl')
-    print(df_final)
     
     # Limpiar hora para tener directamente 00, 01, 02.. 21, 22, 23 etc. en las horas
     df_final['hora'] = df_final['hora_raw'].str.replace('H', '').astype(int) - 1
@@ -68,24 +60,18 @@ def limpiar_datos_csv(ruta_input, ruta_estaciones):
     #df_final = df_final[df_final['es_valido'] == 'V'].copy()
 
     # Mapear nombres de magnitudes
+    df_final['magnitud'] = pd.to_numeric(df_final['magnitud'], errors='coerce')
     df_final['contaminante'] = df_final['magnitud'].map(DICCIONARIO_MAGNITUDES)
-    print('CONTAMINANTES')
-    print(df_final.head())
-    
+    df_final = df_final[df_final['contaminante'].isin(CONTAMINANTES_OBJETIVO)]
+
     # Cruzar datos de estaciones
     df_est_temp = df_est[['CODIGO_CORTO', 'ESTACION']].rename(columns={'ESTACION': 'nombre_estacion'})
-    print('\n')
-    print(df_est_temp.head())
 
     df_final = df_final.merge(df_est_temp, left_on='estacion', right_on='CODIGO_CORTO')
-    print('\n')
-    print(df_final.head())
 
     # Seleccionar columnas finales
     columnas_finales = ['provincia', 'municipio', 'estacion', 'magnitud', 'contaminante','punto_muestreo', 'nombre_estacion', 'ano', 'mes', 'dia', 'hora', 'valor', 'validacion','fecha', 'dia_semana', 'es_fin_semana']
     df_final = df_final[columnas_finales].sort_values(['nombre_estacion', 'fecha'])
-    print('\n')
-    print(df_final.head())
 
     return df_final
 
@@ -136,8 +122,10 @@ def limpiar_datos_live(data_live, ruta_estaciones):
     #df_final = df_final[df_final['es_valido'] == 'V'].copy()
 
     # Mapear nombres de magnitudes
+    df_final['magnitud'] = pd.to_numeric(df_final['magnitud'], errors='coerce')
     df_final['contaminante'] = df_final['magnitud'].map(DICCIONARIO_MAGNITUDES)
-    
+    df_final = df_final[df_final['contaminante'].isin(CONTAMINANTES_OBJETIVO)]
+
     # Cruzar datos de estaciones
     df_est_temp = df_est[['CODIGO_CORTO', 'ESTACION']].rename(columns={'ESTACION': 'nombre_estacion'})
 
